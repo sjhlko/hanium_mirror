c24b3c6e6342795a76bdf7f15ddebc9d97e20312
김민욱 <qjvk2880@naver.com>
2022-07-11 18:22:06 +0900

    🐛 favicon.ico로 인한 중복 요청 버그 해결 및 자료형으로 인한 버그 해결

--- test.js
+++ test.js
@@ -3,43 +3,65 @@ import path from 'path';
 import morgan from 'morgan';
 import request from 'request';
 import convert from 'xml-js';
-import config from './config';
+import config from './config/index.js';
 
 const app = express();
 app.set('port', process.env.PORT || 3001);
 app.use(morgan('dev'));
 
-const ArriveTimekey = config.ArriveTimeKey;
+const ArriveTimekey = config.arriveTimeKey;
 
-app.get('/:station', function (req, res, next) {
-  const apiAddress = encodeURI(
-    `http://swopenAPI.seoul.go.kr/api/subway/${ArriveTimekey}/xml/realtimeStationArrival/0/5/${req.params.station}`,
-  );
-  request(apiAddress, function (error, response, body) {
-    if (error) {
-      console.log(error);
-    }
-
-    const xmlToJson = convert.xml2json(body, { compact: true, spaces: 4 });
-    var data = JSON.parse(xmlToJson).realtimeStationArrival.row;
-    var dataStr = `${req.params.station}<br/>`;
-    for (i in data) {
-      if (
-        true //  data[i].subwayId._text == "1002"
-      ) {
-        console.log(`${data[i].barvlDt._text}초 후 `);
-        // console.log(`${data[i].trainLineNm._text} : ${data[i].barvlDt._text}초 후 `);
-        dataStr += `${data[i].trainLineNm._text} : ${data[i].barvlDt._text}초 후 <br/>`;
+function doRequest(station) {
+  return new Promise(function (resolve, reject) {
+    const apiAddress = encodeURI(
+      `http://swopenAPI.seoul.go.kr/api/subway/${ArriveTimekey}/xml/realtimeStationArrival/0/5/${station}`,
+    );
+    request(apiAddress, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        resolve(body);
+      } else {
+        reject('error');
       }
-    }
-    console.log(JSON.parse(JSON.stringify(data)));
-
-    res.send(dataStr);
+    });
   });
+}
+
+app.get('/', (req, res) => {
+  res.send('초기 페이지입니다.')
+});
+
+app.get('/favicon.ico', (req, res) => {
+  res.status(204);  // favicon.ico으로 인한 중복 요청 방지
+});
+
+app.get('/:station', async function (req, res, next) {
+  console.log('res.json');
+  try {
+    var xmlToJson = convert.xml2json(await doRequest(req.params.station), {
+      compact: true,
+      spaces: 4,
+    });
+  } catch (error) {
+    res.redirect('/');
+  }
+  var data = JSON.parse(xmlToJson);
+  var rowData = data.realtimeStationArrival.row;
+  console.log(rowData);
+  var dataStr = `${req.params.station}<br/>`;
+  if (Array.isArray(rowData)) {  // 도착 정보가 1개일때는 배열이 아니라 객체이기 때문에 조건문 처리함
+    for (let i in rowData) {
+      dataStr += `(호선 번호 : ${rowData[i].subwayId._text}) ${rowData[i].trainLineNm._text} : ${rowData[i].barvlDt._text}초 후 도착<br/>`;
+    }
+  } else {
+    dataStr += `(호선 번호 : ${rowData.subwayId._text}) ${rowData.trainLineNm._text} : ${rowData.barvlDt._text}초 후 도착<br/>`;
+  }
+
+  res.send(dataStr);
 });
 
 
 const rideAlightKey = config.rideAlightKey;
+
 app.get('/:month/:line/:station', function (req, res, next) {
   const myaddr = encodeURI(
     `http://openapi.seoul.go.kr:8088/${rideAlightKey}/xml/CardSubwayTime/1/5/${req.params.month}/${req.params.line}호선/${req.params.station}/`,
