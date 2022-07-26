fa0f1a5a95a7927d69b21e392d7ea332237a406e
sjhlko <76868151+sjhlko@users.noreply.github.com>
2022-07-27 08:02:09 +0900

    🐛 Body Parser 관련 기능 추가

--- loaders/express.js
+++ loaders/express.js
@@ -1,7 +1,11 @@
 import route from '../api/index.js';
 import config from '../config/index.js';
+import express from 'express';
 
 export default async app => {
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
   try {
     app.use(config.api.prefix, route());
     console.log('loading router success!');
