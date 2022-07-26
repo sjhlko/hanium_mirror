dbfd00964964d1396cb3c06a637e659e2bf74c58
sjhlko <76868151+sjhlko@users.noreply.github.com>
2022-07-27 07:57:28 +0900

    ✨ User 관련 함수 추가

--- models/user.js
+++ models/user.js
@@ -3,42 +3,54 @@ const { Model, Sequelize } = _sequelize;
 
 export default class user extends Model {
   static init(sequelize, DataTypes) {
-  return super.init({
-    user_id: {
-      type: DataTypes.STRING(20),
-      allowNull: false,
-      primaryKey: true
-    },
-    speed: {
-      type: DataTypes.DOUBLE(4,2),
-      allowNull: true
-    },
-    age_range: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    gender: {
-      type: DataTypes.CHAR(1),
-      allowNull: true
-    },
-    password: {
-      type: DataTypes.STRING(20),
-      allowNull: true
-    }
-  }, {
-    sequelize,
-    tableName: 'user',
-    timestamps: false,
-    indexes: [
+    return super.init(
       {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "user_id" },
-        ]
+        user_id: {
+          type: DataTypes.STRING(20),
+          allowNull: false,
+          primaryKey: true,
+        },
+        speed: {
+          type: DataTypes.DOUBLE(4, 2),
+          allowNull: true,
+        },
+        age_range: {
+          type: DataTypes.INTEGER,
+          allowNull: true,
+        },
+        gender: {
+          type: DataTypes.CHAR(1),
+          allowNull: true,
+        },
+        password: {
+          type: DataTypes.STRING(20),
+          allowNull: true,
+        },
       },
-    ]
-  });
+      {
+        sequelize,
+        tableName: 'user',
+        timestamps: false,
+        indexes: [
+          {
+            name: 'PRIMARY',
+            unique: true,
+            using: 'BTREE',
+            fields: [{ name: 'user_id' }],
+          },
+        ],
+      },
+    );
+  }
+
+  static async findById(id, attributes) {
+    return await this.findOne({
+      attributes: attributes,
+      where: { user_id: id },
+    });
+  }
+
+  static async createUser(newUser) {
+    return await this.create(newUser);
   }
 }
