db8087afc0345add250005db22c680ffcb52d87a
김민욱 <qjvk2880@naver.com>
2022-07-29 15:55:24 +0900

    🎨 리턴 객체에서 변수명에 언더바 안뜨게 수정

--- library/makeLinesArray.js
+++ library/makeLinesArray.js
@@ -5,8 +5,12 @@ const makeLinesArray = async (stations) => {
     let lines = [];
     for (let i in stations) {
         let lineElement = await lineServiceInstance.getLineByLineId(stations[i].line_id);
-        lineElement.stationId = stations[i].station_id;
-        lines.push(lineElement);
+        let lineObject = {
+            lineName: lineElement.line_name,
+            lineId: lineElement.line_id,
+            stationId:stations[i].station_id,
+        }
+        lines.push(lineObject);
         // const linesObject = {
         //     lineName: await lineServiceInstance.getLineByLineId(stations[i].line_id,),
         //     lineId: stations[i].line_id,
