f3d59bf89148b47f858d3e95a163d58f1eaadd6e
김민욱 <qjvk2880@naver.com>
2022-08-01 03:45:53 +0900

    🔥 역 정보만 조회하는 api 삭제

--- api/routes/station.js
+++ api/routes/station.js
@@ -7,22 +7,6 @@ const stationServiceInstance = new StationService();
 export default app => {
   app.use('/stations', route);
 
-  // 역 정보만 확인
-  route.get(
-    '/:stationIdOrName',
-    middlewares.inCaseStationName,
-    middlewares.inCaseStationId,
-    async (req, res) => {
-      const station = {
-        stationName: req.stationName,
-        lines: req.lines
-      }
-      console.log(station);
-      res.json(station);
-    },
-  );
-
-
   // 역 정보 및 도착정보 확인
   route.get(
     '/arrive-info/:stationId',
