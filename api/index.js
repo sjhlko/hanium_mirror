a7722655aab547fc93e95bf21d69c31a60bff576
sjhlko <76868151+sjhlko@users.noreply.github.com>
2022-08-20 05:23:08 +0900

    🔥 auth 라우터 관련 파일 삭제

--- api/index.js
+++ api/index.js
@@ -1,12 +1,10 @@
 import { Router } from 'express';
 import user from './routes/user.js';
-import auth from './routes/auth.js';
-import station from './routes/station.js'
+import station from './routes/station.js';
 
 export default () => {
   const app = Router();
   user(app);
-  auth(app);
   station(app);
   return app;
 };
